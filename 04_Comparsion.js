d400c8b5e25abfa421e1faecfa4cf405f6bc1981
Yash Dubey <yashdubey20@gmail.com>
2024-01-14 01:50:35 +0530

    Day-2

---
+++ 04_Comparsion.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); // True
+// console.log("02" > 1); // True
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // True
+
+console.log(undefined == 0); // False in all
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === check types
+
+console.log("2" === 2);
