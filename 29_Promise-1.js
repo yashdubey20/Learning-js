59bc7c7c449a942871a18fd2e6ca7e195a0f44fb
Yash Dubey <yashdubey20@gmail.com>
2024-01-27 13:51:25 +0530

    Promises

--- 29_Promise-1.js
+++ 29_Promise-1.js
@@ -1,3 +1,5 @@
+const { json } = require("express")
+
 const promiseOne = new Promise(function(resolve, reject){
     //Do an async task
     // DB calls, cryptography, network
@@ -29,3 +31,66 @@ const promiseThree = new Promise(function(resolve,reject){
 promiseThree.then(function(user) {
     console.log(user)
 })
+
+const promiseFour = new Promise(function(resolve,reject) {
+    setTimeout(function() {
+        let error = true // if false then it wont give an error
+        if (!error) {
+            resolve({username:"Yash Dubey", password:"123"})
+        } else {
+            reject("There seems to be an error")
+        }
+    }, 1000)
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username;
+}).then((username) => {
+    console.log(username)
+}).catch(function(error) {
+    console.log(error)
+}).finally(() => console.log("Finally executed"))
+
+const promiseFive = new Promise(function(resolve,reject) {
+    setTimeout(function() {
+        let error = true 
+        if (!error) {
+            resolve({username:"Js", password:"123"})
+        } else {
+            reject("There seems to be an error in js")
+        }
+    }, 1000)
+})
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+async function getAllUsers() {
+    try {
+        const response = await fetch('https://api.github.com/users/yashdubey20')
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log("Error:", error)
+    }
+}
+
+getAllUsers()
+
+fetch('https://api.github.com/users/yashdubey20')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data)
+})
+.catch((error) => console.log(error))
